6eb570c4b3ba0a02147f6b8cfccaa015d2d308a6
Taaha Bajwa <taaha.s.bajwa@gmail.com>
2023-03-08 18:38:33 +0500

    first

--- main.py
+++ main.py
@@ -36,4 +36,4 @@ async def read_root(request: Request):
     return response
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",host='0.0.0.0', port=8080, reload=True)
+    uvicorn.run("main:app",host='0.0.0.0', port=8000, reload=True)
