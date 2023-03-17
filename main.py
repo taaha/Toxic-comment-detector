b2117a4b30af8990c57575781091cef1b11bfc48
Taaha Bajwa <taaha.s.bajwa@gmail.com>
2023-03-17 17:44:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
-from fastapi import FastAPI,Request, Response
+from fastapi import FastAPI,Request
 # from pydantic import BaseModel
 import uvicorn
 import numpy as np
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, TextClassificationPipeline
+from transformers import BertTokenizer, BertForSequenceClassification
+import torch
 
 app = FastAPI()
 
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
 
 @app.get("/")
-async def root(req: Request):
-    return Response(status_code=200, content="OK")
+def read_root():
+    return {"Hello": "World"}
 
 @app.get("/hello")
 def read_root():
     return {"Hello": "Hello"}
 
 def get_model():
-    tokenizer = AutoTokenizer.from_pretrained("martin-ha/toxic-comment-model")
-    model = AutoModelForSequenceClassification.from_pretrained("martin-ha/toxic-comment-model")
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = BertForSequenceClassification.from_pretrained("pnichite/YTFineTuneBert")
     return tokenizer,model
 
+d = {
+    
+  1:'Toxic',
+  0:'Non Toxic'
+}
+
 tokenizer,model = get_model()
-pipeline =  TextClassificationPipeline(model=model, tokenizer=tokenizer)
 
 @app.post("/predict")
 async def read_root(request: Request):
@@ -32,11 +35,13 @@ async def read_root(request: Request):
     print(data)
     if 'text' in data:
         user_input = data['text']
-        output = pipeline(user_input)
-        response = {"Recieved Text": user_input,"Prediction": output[0]}
+        test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
+        output = model(**test_sample)
+        y_pred = np.argmax(output.logits.detach().numpy(),axis=1)  
+        response = {"Recieved Text": user_input,"Prediction": d[y_pred[0]]}
     else:
         response = {"Recieved Text": "No Text Found"}
     return response
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run("main:app",host='0.0.0.0', port=8080, reload=True, debug=True)
